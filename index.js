20d1d340aee85af666766639b9b9e06e32a8ceca
iparainnocent <innocent.ipara@student.moringaschool.com>
2024-06-27 17:17:57 +0300

    add index.js, package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, surprise){
+    let thankYouMessages = [];
+
+    for (let i = 0; 
+
+        i < names.length; i++){
+            thankYouMessages.push (`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+        }
+        return thankYouMessages;
+}
+function countDown(number){
+    while(number >=0){
+        console.log(number)
+        number --;
+    }
+}
